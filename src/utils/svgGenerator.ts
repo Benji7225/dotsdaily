0f54ac2821097bc7def0c13f88ee821ef8200dd7
benjamindu7 <benjaminezidu7@gmail.com>
2026-01-20 22:10:32 +0100

    Updated svgGenerator.ts

--- src/utils/svgGenerator.ts
+++ src/utils/svgGenerator.ts
@@ -285,7 +285,7 @@ export function generateSVG(config: WallpaperConfig, modelSpecs: ModelSpecs): st
 
   const safeTop = safeArea.top;
   const safeBottom = safeArea.bottom;
-  const safeLeft = safeArea.left + 15;
+  const safeLeft = safeArea.left + 3;
   const safeRight = safeArea.right;
 
   const { current, total } = calculateProgress(config);
