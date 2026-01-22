68797432082ab282dce4ab7fcea5aa9509e2398e
bezi7225 <b.ezi7225@gmail.com>
2026-01-22 11:41:34 +0100

    Updated svgGenerator.ts

--- src/utils/svgGenerator.ts
+++ src/utils/svgGenerator.ts
@@ -308,7 +308,7 @@ export function generateSVG(config: WallpaperConfig, modelSpecs: ModelSpecs): st
 
   let dots = '';
 
-  const dotSpacing = 2;
+  const dotSpacing = 1.8;
 
   const maxMonthDayDotSize = (() => {
     const calendarCols = 7;
