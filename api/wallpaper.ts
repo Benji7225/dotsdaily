4d9895cf082826b84e30f8645e032c1a853023f2
eziz7225 <b.eziz7225@gmail.com>
2026-01-24 20:17:03 +0100

    Updated wallpaper.ts

--- api/wallpaper.ts
+++ api/wallpaper.ts
@@ -1,7 +1,7 @@
 import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { createClient } from '@supabase/supabase-js';
 import { Resvg } from '@resvg/resvg-js';
-import { getRobotoRegular } from './fonts';
+import { getRobotoRegular } from './fonts.js';
 
 interface WallpaperConfig {
   mode: string;
