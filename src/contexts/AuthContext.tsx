ee2b2a86b0d3bdac5774e310d9c467183e66b119
thekrow77 <contact@thekrow.app>
2026-01-28 14:56:58 +0100

    Updated AuthContext.tsx

--- src/contexts/AuthContext.tsx
+++ src/contexts/AuthContext.tsx
@@ -43,7 +43,7 @@ export function AuthProvider({ children }: { children: ReactNode }) {
     const { error } = await supabase.auth.signInWithOAuth({
       provider: 'google',
       options: {
-        redirectTo: `${window.location.origin}/generator`,
+        redirectTo: window.location.href,
       },
     });
 
